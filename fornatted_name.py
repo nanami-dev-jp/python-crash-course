f3f0162fb46124738c65fa9bfef861523ab2cb1f
nanami-dev-jp <pattsun0416@gmail.com>
2026-01-25 18:44:46 +0900

    update

--- fornatted_name.py
+++ fornatted_name.py
@@ -1,7 +1,14 @@
-def get_fornatted_name(first_name, last_name):
+def get_fornatted_name(first_name, last_name, middle_name=''):
     """Return a full name, neatly formatted."""
-    full_name = f"{first_name} {last_name}"
+    if middle_name:
+        full_name = f"{first_name} {middle_name} {last_name}"
+    else:
+        full_name = f"{first_name} {last_name}"
     return full_name.title()
 
 musician = get_fornatted_name('jimi', 'hendrix')
 print(musician)
+
+musician = get_fornatted_name('jimi', 'hooker', 'hendrix')
+print(musician)
+
